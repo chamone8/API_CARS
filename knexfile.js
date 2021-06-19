dd73ce7e6baac93397966d5926153c81388c5878
Info FelipeBraga <info.felipebraga@unidas.com.br>
2021-06-18 23:04:12 -0300

    feat: projeto estava com um bug no sqlite3

--- knexfile.js
+++ knexfile.js
@@ -22,7 +22,7 @@ module.exports = {
       max: 10
     },
     migrations: {
-      // tableName: 'knex_migrations',
+      tableName: 'knex_migrations',
       directory: './src/database/migration/'
     },
     
--- src/database/connection.js
+++ src/database/connection.js
@@ -1,6 +1,6 @@
 const knex = require('knex'); //para conectar o banco de dado
 const configurate = require('../../knexfile');
 
-const connection = knex(configurate.development)//parametro de conexão
+const connection = knex(configurate.development);//parametro de conexão
 
 module.exports = connection;
